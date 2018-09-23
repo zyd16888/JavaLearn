426313c9424ba3bb8c1c495d70adec93d6bbb719
zyd16888 <zyd475182659@163.com>
2018-09-23 16:29:49 +0800

    测试Person重写的Object类

---
+++ 14.x/src/PersonTest.java
@@ -0,0 +1,16 @@
+import java.sql.SQLOutput;
+
+/**
+ * 描述:
+ * 测试Person重写的Object类
+ *
+ * @author dong
+ * @date 2018-09-23 16:26
+ */
+public class PersonTest {
+    public static void main(String[] args) {
+        Person p1 = new Person("张三", 20);
+        Person p2 = new Person("张三", 20);
+        System.out.println(p1.equals(p2));
+    }
+}
