789295232ef2a94ba6a886bcbd5956aebead3ade
zyd16888 <zyd475182659@163.com>
2018-09-24 17:31:10 +0800

    无参数实例化Student类

--- 13.x/src/Student.java
+++ 13.x/src/Student.java
@@ -1,8 +1,15 @@
 public class Student extends Person{
     private double grade;
+    private String schoolName;
 
-    public Student(String name, int age, double grade) {
+    public Student(){
+        System.out.println("无参数实例化Student类完成");
+    }
+
+    public Student(String name, int age, double grade,String schoolName) {
         super(name,age);
         this.grade = grade;
+        this.schoolName = schoolName;
+        System.out.println("有参数实例化Student类完成");
     }
 }
