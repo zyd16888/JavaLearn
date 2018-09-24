7a2d3d33934369f4b81e30870023757b0fdff2be
zyd16888 <zyd475182659@163.com>
2018-09-24 18:00:32 +0800

    工人类继承于Person

---
+++ 13.x/src/Worker.java
@@ -0,0 +1,20 @@
+import java.awt.*;
+
+/**
+ * 描述:
+ * 工人类
+ *
+ * @author dong
+ * @date 2018-09-24 17:57
+ */
+public class Worker extends Person {
+    private int salary;
+    private String grade;
+
+    public Worker(String name, int age, int salary, String grade) {
+        super(name, age);
+        this.salary = salary;
+        this.grade = grade;
+    }
+
+}
