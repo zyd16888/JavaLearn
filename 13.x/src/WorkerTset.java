6af38c46551ded6ccc35a9d7acda47b95ae8cbf8
zyd16888 <zyd475182659@163.com>
2018-09-24 18:08:19 +0800

    Worker测试类

---
+++ 13.x/src/WorkerTset.java
@@ -0,0 +1,13 @@
+/**
+ * 描述:
+ * Worker测试类
+ *
+ * @author dong
+ * @date 2018-09-24 18:01
+ */
+public class WorkerTset {
+    public static void main(String[] args) {
+        Worker w = new Worker("张三",25,5600,"总工");
+        w.display();
+    }
+}
