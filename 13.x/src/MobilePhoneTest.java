cff8a1c6708bb0d973a322177b1288be25fa3c43
zyd16888 <zyd475182659@163.com>
2018-09-24 18:25:07 +0800

    手机类测试

---
+++ 13.x/src/MobilePhoneTest.java
@@ -0,0 +1,17 @@
+/**
+ * 描述:
+ * 手机类测试
+ *
+ * @author dong
+ * @date 2018-09-24 18:20
+ */
+public class MobilePhoneTest {
+    public static void main(String[] args) {
+        MobilePhone  m = new  MobilePhone("13700000000","三星","142256188705064589");
+        m.display();
+        System.out.println("话费为："+m.pay(20,0.1));
+
+    }
+
+
+}
