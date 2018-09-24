77bec805a7599534792d37050ab5e34802e4bbd6
zyd16888 <zyd475182659@163.com>
2018-09-24 17:28:05 +0800

    创建无参数构造方法

--- 13.x/src/Person.java
+++ 13.x/src/Person.java
@@ -10,8 +10,14 @@ public class Person {
         this.age = age;
     }
 
+    public Person(){
+        this("",0);
+        System.out.println("无参数实例化Person类完成");
+    }
+
     public void display(){
         System.out.println("姓名："+name);
         System.out.println("年龄："+age);
+        System.out.println("有参数实例化Person类完成");
     }
 }
