6c5427d9c315df818c2a9ccd3bce049f7b0766ea
zyd16888 <zyd475182659@163.com>
2018-09-24 17:25:19 +0800

    教师类测试

---
+++ 13.x/src/TeacherTest.java
@@ -0,0 +1,13 @@
+/**
+ * 描述:
+ * 教师类测试
+ *
+ * @author dong
+ * @date 2018-09-24 17:24
+ */
+public class TeacherTest {
+    public static void main(String[] args) {
+        Teacher zhang = new Teacher("张老师",40,4580,"副教授");
+        zhang.display();
+    }
+}
