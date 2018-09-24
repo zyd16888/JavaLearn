3e3e3463be7b0ad0a990c5749c219c65d3b71616
zyd16888 <zyd475182659@163.com>
2018-09-24 16:38:34 +0800

    取消 定义public属性name2

--- 14.x/src/Student.java
+++ 14.x/src/Student.java
@@ -1,13 +1,7 @@
 public class Student extends Person{
     private double grade;
-    public String name2 = "李四";
 
-    @Override
-    public String getName2() {
-        return name2;
-    }
-
-    public Student (String name, int age, double grade){
+    public Student (String name,int age,double grade){
         super(name, age);
         this.grade = grade;
     }
