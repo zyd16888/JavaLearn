800f92c8eaae09b80275059ce7f1c34536fca4ba
zyd16888 <zyd475182659@163.com>
2018-09-25 08:04:04 +0800

    14.5.2题

---
+++ 14.x/src/WorkerTest.java
@@ -0,0 +1,13 @@
+/**
+ * 描述:
+ * Worker测试类
+ *
+ * @author dong
+ * @date 2018-09-24 18:01
+ */
+public class WorkerTest {
+    public static void main(String[] args) {
+        Person w = new Worker("张三",25,5600,"总工");
+        w.display();
+    }
+}
