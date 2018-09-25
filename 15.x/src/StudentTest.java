55a88d17e2893e7f47ed6bda5d5131e442feef35
zyd16888 <zyd475182659@163.com>
2018-09-25 08:34:06 +0800

    Student测试类

---
+++ 15.x/src/StudentTest.java
@@ -0,0 +1,13 @@
+/**
+ * 描述:
+ * Student测试类
+ *
+ * @author dong
+ * @date 2018-09-25 8:30
+ */
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s = new Student("张三",23,86);
+        s.display();
+    }
+}
