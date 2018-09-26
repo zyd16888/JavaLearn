7222726d5528eba279315dd22ee617b08a158732
zyd16888 <zyd475182659@163.com>
2018-09-26 16:47:29 +0800

    增加程序序号

--- 16.x/src/CalculateAble.java
+++ 16.x/src/CalculateAble.java
@@ -1,3 +1,10 @@
+/**
+ * 描述:
+ * 接口测试类
+ * 16.9
+ * @author dong
+ * @date 2018-09-25 21:30
+ */
 public interface CalculateAble {
     public double getArea();
 }
--- 16.x/src/Circle2.java
+++ 16.x/src/Circle2.java
@@ -1,7 +1,7 @@
 /**
  * 描述:
  * 实现接口CalculateAble的圆类
- *
+ * 16.7
  * @author dong
  * @date 2018-09-25 21:52
  */
