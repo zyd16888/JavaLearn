85fcacc3999c3702d418ba03337d270beb320e7d
zyd16888 <zyd475182659@163.com>
2018-09-27 11:20:24 +0800

    16.5.2题

---
+++ 16.x/src/Teacher2.java
@@ -0,0 +1,23 @@
+/**
+ * 描述:
+ * Teacher类实现SoundAble接口
+ * 16.5.2题
+ *
+ * @author dong
+ * @date 2018-09-27 11:13
+ */
+public class Teacher2 extends Person implements SoundAble {
+    public Teacher2(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("正在讲课！");
+    }
+}
---
+++ 16.x/src/Worker2.java
@@ -0,0 +1,23 @@
+/**
+ * 描述:
+ * Worker类实现SoundAble接口
+ * 16.5.2题
+ *
+ * @author dong
+ * @date 2018-09-27 11:17
+ */
+public class Worker2 extends Person implements SoundAble {
+    public Worker2(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public void display() {
+
+    }
+
+    @Override
+    public void sound() {
+        System.out.println("噪声太大听不清楚！");
+    }
+}
