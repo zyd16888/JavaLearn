434847567b3a58b9e373a702e12839a859d4128b
zyd16888 <zyd475182659@163.com>
2018-10-09 08:48:23 +0800

    commit

---
+++ 11.x/src/Student.java
@@ -0,0 +1,64 @@
+/**
+ * 描述:
+ * 学生类
+ * 11.2
+ *
+ * @author dong
+ * @date 2018-10-09 8:32
+ */
+public class Student {
+    private String name;
+    private int age;
+    private double grade;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public Student() {
+        this.name = "";
+        this.age = 0;
+        this.grade = 0;
+    }
+
+    public Student(String name, int age, double grade) {
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+    }
+
+    public boolean beSame(String name){
+        return name.equals(this.name);
+    }
+
+    public boolean beSame(String name,int age){
+        boolean flag = name.equals(this.name);
+        flag = flag && (age == this.age);
+        return flag;
+    }
+
+    public boolean beSame(){
+        return Boolean.parseBoolean(null);
+    }
+
+}
---
+++ 11.x/src/StudentTest.java
@@ -0,0 +1,14 @@
+/**
+ * 描述:
+ * 学生测试类
+ *
+ * @author dong
+ * @date 2018-10-09 8:45
+ */
+public class StudentTest {
+    public static void main(String[] args) {
+        Student s1 = new Student("张三",23,74);
+        Student s2 = new Student("张三1",23,74);
+        s1.beSame(s2.getName());
+    }
+}
