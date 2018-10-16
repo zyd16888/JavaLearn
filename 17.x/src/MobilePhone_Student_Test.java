6f2474548f5b0b5f87cb8a31ca8fb74b45c203f8
zyd16888 <zyd475182659@163.com>
2018-10-16 20:51:21 +0800

    运行异常处理

---
+++ 17.x/src/MobilePhone_Student_Test.java
@@ -0,0 +1,15 @@
+/**
+ * 17.3
+ * 异常处理
+ *
+ * @author dong
+ */
+public class MobilePhone_Student_Test {
+    public static void main(String[] args) {
+        MobilePhone phone = new MobilePhone("Apple","13800000000");
+        Student s = new Student("张三",23,74,phone);
+        s.display();
+           System.out.println("``````````````");
+        phone.print();
+    }
+}
---
+++ 17.x/src/Student.java
@@ -0,0 +1,27 @@
+/**
+ * 17.2
+ *
+ * @author dong
+ */
+public class Student {
+    private String name;
+    private int age;
+    private double grade;
+    private MobilePhone myphone;
+
+    public Student(String name,int age,double grade,MobilePhone myphone){
+        this.name = name;
+        this.age = age;
+        this.grade = grade;
+        this.myphone = myphone;
+        this.myphone.setOwner(null);
+
+    }
+
+    public void display(){
+        System.out.println("姓名："+name);
+        System.out.println("年龄："+age);
+        System.out.println("成绩："+grade);
+//        myphone.print();
+    }
+}
