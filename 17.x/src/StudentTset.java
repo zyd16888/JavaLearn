a6fabc22811f04a74bbf78cd79500ca9f004bc84
zyd16888 <zyd475182659@163.com>
2018-10-18 20:18:58 +0800

    17.11 对输入的错误数据进行异常处理 允许用户修改数据

--- 17.x/src/StudentTset.java
+++ 17.x/src/StudentTset.java
@@ -14,15 +14,26 @@ import java.util.Scanner;
 public class StudentTset {
     public static void main(String[] args) {
         Student s = new Student("张三",23,74);
-        Scanner sc = new Scanner(System.in);
+
         int age = 0;
+        while (age == 0){
+            age = input();
+        }
+        s.setAge(age);
+        s.display();
+    }
+
+    public static int input(){
+        int age = 0;
+        Scanner sc = new Scanner(System.in);
+
         try {
             age = sc.nextInt();
         }
         catch (InputMismatchException ime){
             System.out.println("输入数据格式错误！");
         }
-        s.setAge(age);
-        s.display();
+
+        return age;
     }
 }
