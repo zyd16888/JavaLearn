965c86d797288146b2fa6689a590cc9d110f2293
zyd16888 <zyd475182659@163.com>
2018-10-28 18:24:35 +0800

    20.3

--- 20.x/src/TestNew.java
+++ 20.x/src/TestNew.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Method;
  * 描述:
  * 装入类程序
  * 20.1
+ * 20.3
  *
  * @author dong
  * @date 2018-10-28 17:57
@@ -14,14 +15,21 @@ public class TestNew {
         String methodName = "display";
 
         try {
-            Class para[] = new Class[0];
-            Object ob[] = new Object[0];
 
             Class theObject = Class.forName(className);
             Object theInst = (Object) theObject.newInstance();
 
+            Class zs[] = new Class[1];
+            zs[0] = String.class;
+            Method setZs = theObject.getMethod(new String("setName"),zs);
+            Object zsPara[] = new Object[1];
+            zsPara[0] = new String("张三");
+            Object returnObject = setZs.invoke(theInst,zsPara);
+
+            Class para[] = new Class[0];
+            Object ob[] = new Object[0];
             Method theMethod = theObject.getMethod(methodName,para);
-            Object returnObject = theMethod.invoke(theInst,ob);
+            returnObject = theMethod.invoke(theInst,ob);
         }catch (Throwable e){
             System.err.print(e);
         }
