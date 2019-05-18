cd372fa1851e0900420600df140bdbfbe19381f0
zyd16888 <zyd475182659@163.com>
2019-05-18 12:47:25 +0800

    主函数

---
+++ 23.x/src/Main.java
@@ -0,0 +1,16 @@
+import view.StudentManagement;
+
+/**
+ * 描述:
+ * 窗口测试类
+ * 23.2
+ *
+ * @author dong
+ * @date 2018-11-18 11:50
+ */
+public class Main {
+    public static void main(String[] args) {
+        StudentManagement sm = new StudentManagement("学生成绩管理");
+        sm.run();
+    }
+}
